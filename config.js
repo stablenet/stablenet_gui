28e9d75ae6c5ca9379bac329b9d9cb7175531e84
steady.net <netsteady9@gmail.com>
2024-12-27 21:12:22 -0500

    replace api

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.config={
     logo: '/assets/logo.png', // 网站logo
     title:"稳联云", // 网站标题
-    host:"https://api.stablenet.top/", // 后端接口地址
+    host:"https://board2.stablenet.top/", // 后端接口地址
     storeHome: {
         title: '选择最适合你的订阅计划', // 套餐页标题
         description: '若您已购买订阅且当前未过期，再次购买相同套餐将在原过期时间上追加时长，购买其他套餐则对原有订阅进行折抵' // 套餐页描述
